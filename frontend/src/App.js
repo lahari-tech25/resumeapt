2a91bbbdfa283e08c4204cad998416c12713b30c
lahari-tech25 <lahari7625@gmail.com>
2025-10-17 20:17:22 +0530

    Convert frontend to normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-2xl font-bold">ResumeApt Frontend Running!</h1>
+    </div>
+  );
+}
+
+export default App;

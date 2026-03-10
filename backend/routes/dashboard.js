344946a348c09c81a49331d58a481d47e7fd9537
lahari-tech25 <lahari7625@gmail.com>
2026-03-10 16:07:22 +0530

    issue fixed

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getDashboardStats } from "../controllers/dashboardController.js";
-import verifyToken  from "../middleware/authMiddleware.js";
+import verifyToken  from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
